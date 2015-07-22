d41b327f45d1a0aeb95f22d3fda50096d1a77542
Justinian Huntz <justinhunter1@live.com>
2015-07-21 20:31:20 -0400

    new workins

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name astoreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the astoreApp
+ */
+angular.module('astoreApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
